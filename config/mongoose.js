736790790ce549b808f391bdfcef46324d77597a
Amrit Chattopadhyay <amritchattopadhyay456@gmail.com>
2023-01-19 21:56:59 +0530

    all

---
+++ config/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/codeial_development');
+const db=mongoose.connection;
+db.on('err',console.error.bind(console,"error connecting to mongodb"));
+db.once('open',function(){
+    console.log('connected to db :: MongoDB');
+});
+module.exports=db;
--- controllers/home_controler.js
+++ controllers/home_controler.js
@@ -1,4 +1,6 @@
 module.exports.home=function(req,res){
+    res.cookie('user_id',25);
+    console.log(req.cookies);
     //return res.end('<h1>express is up for codeial</h1>');
     return res.render('home',{
         title:"Home"
