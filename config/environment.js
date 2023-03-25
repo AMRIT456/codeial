57de26f2dd3f6f8674168be122e1e4ae8c73a387
Amrit Chattopadhyay <amritchattopadhyay456@gmail.com>
2023-03-25 22:07:22 +0530

    all

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
 
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d',
+    path: logDirectory
+});
 
 const development = {
     name: 'development',
     asset_path: '/assets',
-    session_cookie_key: 'blahsomething',
+    session_cookie_key: 'h]ro#=)C-Zl<YqPdk:@QVGcbl@j`+R',
     db: 'codeial_development',
     smtp: {
         service: 'gmail',
@@ -18,14 +28,40 @@ const development = {
     google_client_id: "151318013613-ghn1fcduqqj3ktviqqcp1cvv7ll0jndu.apps.googleusercontent.com",
     google_client_secret: "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",
     google_call_back_url: "http://localhost:8000/users/auth/google/callback",
-    jwt_secret: 'codeial',
+    jwt_secret: 'uoZTu1EODj3NdMKyAhPb0jSls7nBzKyf',
+    morgan: {
+        mode: 'dev',
+        options: {stream: accessLogStream}
+    }
 }
 
 
-const production =  {
-    name: 'production'
+const production = {
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH, 
+    //asset_path: './assets', 
+    session_cookie_key:process.env.CODEIAL_SESSION_COOKIE,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id:process.env.GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.GOOGLE_CALLBACK_RURL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET,
+    morgan: {
+        mode: 'combined',
+        options: {stream: accessLogStream}
+    }
 }
 
 
-
-module.exports = development;
+// module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
